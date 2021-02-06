ccdd92ca8b59be29449a4dadf4ee3cb102010fb0
eriklangille <eriklangille@gmail.com>
2021-02-06 14:57:55 -0800

    Added beginning of Project

--- showfolio/src/App.tsx
+++ showfolio/src/App.tsx
@@ -2,12 +2,24 @@ import React from 'react'
 import './App.scss';
 import Header from './components/Header';
 import Profile from './components/Profile';
+import Project from './components/Project';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Profile />
+      <Profile 
+        Name="Scott Langille" 
+        Description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore." 
+        Avatar={process.env.PUBLIC_URL + '/users/scottlangille/avatar.jpg'}
+      />
+      <Project 
+        Name="uOttaHacks4"
+        Image={process.env.PUBLIC_URL + '/users/scottlangille/uOttahacks/image.png'}
+        ShortDescription="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore." 
+        StartDate={new Date("2021-02-05 8:00")}
+        EndDate={new Date("2021-02-07 8:00")}
+      />
     </div>
   );
 }
