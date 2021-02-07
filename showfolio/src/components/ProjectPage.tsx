9f67c3b855b2b34feab5d145352bf68d9bf189b6
eriklangille <eriklangille@gmail.com>
2021-02-07 02:22:34 -0800

    Dynamic Project Page

--- showfolio/src/components/ProjectPage.tsx
+++ showfolio/src/components/ProjectPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import axios from '../utils/axios'
+import axios, { url } from '../utils/axios'
 import { useParams } from 'react-router-dom'
 
 type Link = {
@@ -7,9 +7,14 @@ type Link = {
   project: string
 }
 
+type SectionProps = {
+  title: string,
+  body: string
+}
+
 const ProjectPage = () => {
 
-  const [projectInfo, setProjectInfo] = useState([]);
+  const [projectInfo, setProjectInfo] = useState();
 
   let { user, project } : Link = useParams();
 
@@ -23,13 +28,62 @@ const ProjectPage = () => {
     })
   }
 
+  const getMainProject = () => {
+    let projects = []
+    if (projectInfo != null) {
+      // console.log("PROJECTINFO")
+      // console.log(projectInfo)
+      try {
+        console.log(projectInfo["Projects"][projectInfo["MainProject"]]);
+        projects = projectInfo["Projects"][projectInfo["MainProject"]]
+      } catch {
+        projects = []
+      }
+    }
+    return projects
+  }
+
+  const getMainImage = () => {
+    let projects = getMainProject()
+    let result = ""
+    if (!(projects instanceof Array)) {
+      try {
+        result = projects["gallery"][0]
+      } catch {
+        result = ""
+      }
+    }
+    return result
+  }
+
   useEffect(() => {
     getProject(user, project)
   }, [user, project]);
 
+  const Section = ({ title, body } : SectionProps) => <div>
+    <h2>{title}</h2>
+    {body.split('\n').map(str => <p>{str}</p>)}
+  </div>
+
   return (
     <div>
-      {JSON.stringify(projectInfo)}
+      {getMainProject() instanceof Array ? null :
+      <div>
+        <div>
+          <img src={`${url}/assets/${getMainImage()}`} />
+          <h1>
+            {getMainProject()["name"]}
+          </h1>
+          <div>
+            <Section title="Summary" body={getMainProject()["info"]["summary"]} />
+            <Section title="Problem" body={getMainProject()["info"]["problem"]} />
+            <Section title="Solution" body={getMainProject()["info"]["solution"]} />
+            <Section title="Implementation" body={getMainProject()["info"]["implementation"]} />
+            <Section title="Outcome" body={getMainProject()["info"]["outcome"]} />
+          </div>
+        </div>
+      </div>
+}
     </div>
   );
 }
--- showfolio/src/utils/axios.tsx
+++ showfolio/src/utils/axios.tsx
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+export const url = "http://localhost:5000";
+
 export default axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: url,
 });
